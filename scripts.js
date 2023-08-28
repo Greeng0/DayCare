1b015e374e0fa80cc604d61cba9b932951b594cd
Greeng0 <eminalex@hotmail.com>
2023-08-28 18:14:25 -0400

    Updates by becca
    - Removed careers and calendar pates
    - Updated staff photos and sizes

--- scripts.js
+++ scripts.js
@@ -4,16 +4,11 @@ var indexViewModel = (function () {
 	
 	self.tab = "home";
 	self.ignoreHashChange = false;
-	self.encodedAttachment = "";
 	
 	self.init = function () {
 		var $document = $(document);
 		var $window = $(window);
 		$document.on("click", "#navbar a, .preview-box", self.navClickHandler);
-		$document.on("click", "#sendBtn", self.sendEmailClickHandler);
-		$document.on("change", "#resume", self.uploadFileToServer);
-		$document.on("fileselect", "#resume", self.fileSelected);
-		$document.on("submit", "#job-form", self.submitApplication);
 		$window.on("hashchange", self.hashChangedHandler);
 		
 		$("#currentYear").text(new Date().getFullYear());
@@ -49,101 +44,6 @@ var indexViewModel = (function () {
 		}
 		var hash = self.convertURLHashToObject().t;
 		self.navClickHandler({ tab: hash || "home" });
-	};
-
-	self.sendEmailClickHandler = function () {
-		var subject = encodeURIComponent(document.getElementById("subject").value);
-		var message = encodeURIComponent(document.getElementById("message").value);
-		var address = encodeURIComponent("mailto:TheMelbournePreschool@gmail.com");
-		window.open("mailto:" + address + "?subject=" + subject + "&body=" + message);
-	};
-	
-	self.uploadFileToServer = function () {
-		var label = $(this).val()
-			.replace(/\\/g, "/")
-			.replace(/.*\//, "");
-		$(this).trigger("fileselect", [label]);
-		
-		var file = event.srcElement.files[0];
-		var reader = new FileReader();
-		reader.readAsBinaryString(file);
-		reader.onload = function () {
-			self.encodedAttachment = "data:" + file.type + ";base64," + btoa(reader.result);
-		};
-		reader.onerror = function() {
-			console.error("There was an error encoding the attachment.");
-		};
-	};
-	
-	self.fileSelected = function (e, label) {
-		$(".custom-file-label").text(label);
-	};
-	
-	self.submitApplication = function (e) {
-		e.preventDefault();
-		e.stopPropagation();
-		  
-		var body = self.buildApplicationBody();
-		
-		var $form = $("#job-form");
-		if ($form.length) {
-			// Invoke form validation
-			var isValid = $form[0].checkValidity();
-			$form.addClass("was-validated");
-			
-			// Wipe the error message label
-			self.clearValidationError($form);
-			
-			if (!self.encodedAttachment) {
-				self.showValidationError($form, "Please attach your resumé!");
-				return false;
-			}
-			
-			if (isValid === false) {
-				self.showValidationError($form, "Please fill out all the fields!");
-				return false;
-			}
-			try {
-				// Show the loading spinner
-				$form.find(".loader").css("display", "block");
-				Email.sendWithAttachment(
-					"TheMelbournePreschool@gmail.com", 					// from
-					"TheMelbournePreschool@gmail.com", 					// to
-					"Job application from TheMelbournePreschool.com", 	// subject
-					body, 												// body
-					{token: "89fb9dc3-e681-46c5-8166-31a2d734730e"},	// token for the SMTP
-					null,												// SMTP username (not used)
-					null,												// SMTP password (not used)
-					self.encodedAttachment,								// Attachment
-					function done(message) { 
-						$form.find(".loader").css("display", "none");
-						alert("Thank you for your application!");
-					}
-				);
-			}
-			catch (e) {
-				$form.find(".loader").css("display", "none");
-			}
-		}
-		return false;
-	};
-	
-	self.showValidationError = function ($form, message) {
-		$form.find(".validation-error")
-			.text(message)
-			.css("display", "");
-	};
-	
-	self.clearValidationError = function ($form, message) {
-		$form.find(".validation-error").text("");
-	};
-	
-	self.buildApplicationBody = function () {
-		var body = "";
-		$("#careers").find("input:not([type=file]):not([type=submit])").each(function () {
-			body += $(this).attr("placeholder") + ": " + $(this).val() + "<br>";
-		});
-		return body;
 	};
 	
 	self.openTab = function () {
@@ -226,8 +126,7 @@ var indexViewModel = (function () {
 	
 	return {
 		initialize: self.init,
-		currentYear: self.currentYear,
-		buildBody: self.buildApplicationBody
+		currentYear: self.currentYear
 	};
 })();
 
